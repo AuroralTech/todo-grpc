a1d52978c123c20f7f1f1c62c7a078e6c5c0f564
sako0 <y.m1911a1.0921@gmail.com>
2023-11-25 17:49:39 +0900

    話し合いの末、インターフェースを追加

--- pkg/usecase/todo_usecase.go
+++ pkg/usecase/todo_usecase.go
@@ -5,26 +5,32 @@ import (
 	"github.com/AuroralTech/todo-grpc/pkg/domain/repository"
 )
 
-type TodoUsecase struct {
+type TodoUsecase interface {
+	AddTodo(todo *model.Todo) (*model.Todo, error)
+	UpdateTodoStatus(id string, isCompleted bool) (bool, error)
+	DeleteTodoById(id string) (bool, error)
+	GetTodoList() ([]*model.Todo, error)
+}
+type todoUsecase struct {
 	repo repository.TodoRepository
 }
 
 func NewTodoUsecase(repo repository.TodoRepository) TodoUsecase {
-	return TodoUsecase{repo}
+	return &todoUsecase{repo}
 }
 
-func (u *TodoUsecase) AddTodo(todo *model.Todo) (*model.Todo, error) {
+func (u *todoUsecase) AddTodo(todo *model.Todo) (*model.Todo, error) {
 	return u.repo.AddTodo(todo)
 }
 
-func (u *TodoUsecase) UpdateTodoStatus(id string, isCompleted bool) (bool, error) {
+func (u *todoUsecase) UpdateTodoStatus(id string, isCompleted bool) (bool, error) {
 	return u.repo.UpdateTodoStatus(id, isCompleted)
 }
 
-func (u *TodoUsecase) DeleteTodoById(id string) (bool, error) {
+func (u *todoUsecase) DeleteTodoById(id string) (bool, error) {
 	return u.repo.DeleteTodoById(id)
 }
 
-func (u *TodoUsecase) GetTodoList() ([]*model.Todo, error) {
+func (u *todoUsecase) GetTodoList() ([]*model.Todo, error) {
 	return u.repo.GetTodoList()
 }
