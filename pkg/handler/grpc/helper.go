7abfc868a8a3dd3f7071241d7fa2b07e80b292b2
fuku01 <yfu904@gmail.com>
2023-12-08 02:04:01 +0900

    firebase authの処理追加

--- pkg/handler/grpc/helper.go
+++ pkg/handler/grpc/helper.go
@@ -2,19 +2,44 @@ package grpc
 
 import (
 	"context"
-	"fmt"
+	"log"
+	"strings"
 
+	firebase "firebase.google.com/go"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/status"
 )
 
 func Authenticate(ctx context.Context) error {
+	// 1.gRPCのメタデータからJWTトークンを取得
 	md, _ := metadata.FromIncomingContext(ctx)
 	auth := md.Get("authorization")
 	if len(auth) == 0 {
 		return status.Error(codes.Unauthenticated, "authorization token is not provided")
 	}
-	fmt.Println(auth[0]) // JWTトークンをログ出力
+	// 2. Bearerを除くトークンを取得
+	idToken := strings.TrimPrefix(auth[0], "Bearer ")
+
+	// 3.Firebase SDKの初期化
+	app, err := firebase.NewApp(context.Background(), nil)
+	if err != nil {
+		log.Fatalf("error initializing app: %v\n", err)
+	}
+
+	// 4.認証クライアントを取得
+	client, err := app.Auth(ctx)
+	if err != nil {
+		log.Fatalf("error getting Auth client: %v\n", err)
+	}
+
+	// 5.トークンを検証
+	token, err := client.VerifyIDToken(ctx, idToken)
+	if err != nil {
+		log.Fatalf("error verifying ID token: %v\n", err)
+	}
+
+	log.Printf("Verified ID token: %v\n", token)
+
 	return nil
 }
